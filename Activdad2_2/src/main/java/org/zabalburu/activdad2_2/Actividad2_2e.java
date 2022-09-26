925314c81b660ff37d5129b58f55291c077b8ccb
Junior Uzuriaga <junior.uzu5@gmail.com>
2022-09-26 18:55:56 +0200

    Actividad 2 corrección

--- Activdad2_2/src/main/java/org/zabalburu/activdad2_2/Actividad2_2e.java
+++ Activdad2_2/src/main/java/org/zabalburu/activdad2_2/Actividad2_2e.java
@@ -4,6 +4,8 @@
  */
 package org.zabalburu.activdad2_2;
 
+import javax.swing.JOptionPane;
+
 /**
  * e.- Recoger dos valores del teclado en dos variables diferentes, intercambiar
  * sus contenidos y finalmente visualizarlos.
@@ -11,5 +13,19 @@ package org.zabalburu.activdad2_2;
  * @author Junior
  */
 public class Actividad2_2e {
-
+   public static void main(String[] args) {
+        String resp = JOptionPane.showInputDialog("Introduza el primer valor entero");
+        int num1 = Integer.parseInt(resp);
+        resp = JOptionPane.showInputDialog("Introduza el segundo valor entero");
+        int num2 = Integer.parseInt(resp);
+        double producto = num1 * num2;
+        double suma = num1 + num2;
+        double media = suma / 2;
+        JOptionPane.showMessageDialog(null,
+                num1 + " * " + num2 + " = " + producto + "\n" +
+                num1 + " + " + num2 + " = " + suma + "\n" +
+                "Media de " + num1 + " y " + num2 + " = " + media + "\n");
+    }
+    
 }
+
